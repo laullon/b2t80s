71b38d2d9eff65e4533a455336650a7680c8b856
German Laullon <laullon@gmail.com>
2020-12-11 19:17:53 +0100

    take 8

--- z80/z80_scheduler.go
+++ z80/z80_scheduler.go
@@ -22,7 +22,7 @@ func (op *basicOp) isDone() bool {
 	return op.done
 }
 
-type z80f func(*z80, []uint8)
+type z80f func(*z80)
 
 // -------------------------------------------------------------
 
@@ -43,10 +43,12 @@ func (ops *fetch) tick(cpu *z80) {
 		cpu.regs.M1 = false
 		cpu.bus.ReadMemory()
 		d := cpu.bus.GetData()
-		cpu.fetched = append(cpu.fetched, d)
 		cpu.opBytes = append(cpu.opBytes, d)
+		cpu.fetched.prefix = cpu.fetched.prefix << 8
+		cpu.fetched.prefix |= uint16(cpu.fetched.opCode)
+		cpu.fetched.opCode = d
 	case 4:
-		op := ops.table[cpu.fetched[len(cpu.fetched)-1]]
+		op := ops.table[cpu.fetched.opCode]
 		if op == nil {
 			panic(errors.Errorf("opCode '0x%X' not found", cpu.opBytes))
 		}
@@ -55,7 +57,7 @@ func (ops *fetch) tick(cpu *z80) {
 		}
 		cpu.scheduler.append(op.ops...)
 		if op.onFetch != nil {
-			op.onFetch(cpu, cpu.fetched)
+			op.onFetch(cpu)
 		}
 		ops.done = true
 	}
@@ -72,14 +74,14 @@ func newFetch(table []*opCode) *fetch {
 
 // -------------------------------------------------------------
 
-type mrPC struct {
+type mrNpc struct {
 	basicOp
 	f z80f
 }
 
-func (ops *mrPC) tick(cpu *z80) {
+func (ops *mrNpc) tick(cpu *z80) {
 	ops.t++
-	// println("> [mrPC]", ops.t, "pc:", fmt.Sprintf("0x%04X", cpu.regs.PC))
+	// println("> [mrNpc]", ops.t, "pc:", fmt.Sprintf("0x%04X", cpu.regs.PC))
 	switch ops.t {
 	case 1:
 		cpu.bus.SetAddr(cpu.regs.PC)
@@ -87,10 +89,45 @@ func (ops *mrPC) tick(cpu *z80) {
 	case 3:
 		cpu.bus.ReadMemory()
 		d := cpu.bus.GetData()
-		cpu.fetched = append(cpu.fetched, d)
 		cpu.opBytes = append(cpu.opBytes, d)
+		cpu.fetched.n = d
 		if ops.f != nil {
-			ops.f(cpu, cpu.fetched)
+			ops.f(cpu)
+		}
+		ops.done = true
+	}
+}
+
+// -------------------------------------------------------------
+
+type mrNNpc struct {
+	basicOp
+	f z80f
+}
+
+func (ops *mrNNpc) tick(cpu *z80) {
+	ops.t++
+	// println("> [mrNNpc]", ops.t, "pc:", fmt.Sprintf("0x%04X", cpu.regs.PC))
+	switch ops.t {
+	case 1:
+		cpu.bus.SetAddr(cpu.regs.PC)
+		cpu.regs.PC++
+	case 3:
+		cpu.bus.ReadMemory()
+		d := cpu.bus.GetData()
+		cpu.opBytes = append(cpu.opBytes, d)
+		cpu.fetched.n = d
+	case 4:
+		cpu.bus.SetAddr(cpu.regs.PC)
+		cpu.regs.PC++
+	case 6:
+		cpu.bus.ReadMemory()
+		d := cpu.bus.GetData()
+		cpu.opBytes = append(cpu.opBytes, d)
+		cpu.fetched.n2 = d
+		cpu.fetched.nn = uint16(cpu.fetched.n) | (uint16(cpu.fetched.n2) << 8)
+		if ops.f != nil {
+			ops.f(cpu)
 		}
 		ops.done = true
 	}
@@ -99,9 +136,10 @@ func (ops *mrPC) tick(cpu *z80) {
 //
 // -------------------------------------------------------------
 
+type z80MRf func(cpu *z80, data byte)
 type mr struct {
 	basicOp
-	f    z80f
+	f    z80MRf
 	from uint16
 }
 
@@ -114,7 +152,7 @@ func (ops *mr) tick(cpu *z80) {
 		cpu.bus.ReadMemory()
 		d := cpu.bus.GetData()
 		if ops.f != nil {
-			ops.f(cpu, []byte{d})
+			ops.f(cpu, d)
 		}
 		ops.done = true
 	}
@@ -122,7 +160,7 @@ func (ops *mr) tick(cpu *z80) {
 
 var mrsPool = newObjectPool(func() interface{} { return &mr{} })
 
-func newMR(from uint16, f z80f) *mr {
+func newMR(from uint16, f z80MRf) *mr {
 	mr := mrsPool.next().(*mr)
 	mr.reset()
 	mr.from = from
@@ -133,9 +171,11 @@ func newMR(from uint16, f z80f) *mr {
 //
 // -------------------------------------------------------------
 
+type z80INf func(cpu *z80, data byte)
+
 type in struct {
 	basicOp
-	f    z80f
+	f    z80INf
 	from uint16
 }
 
@@ -153,7 +193,7 @@ func (ops *in) tick(cpu *z80) {
 		cpu.regs.F.P = parityTable[data]
 		cpu.regs.F.N = false
 		if ops.f != nil {
-			ops.f(cpu, []byte{data})
+			ops.f(cpu, data)
 		}
 		ops.done = true
 	}
@@ -165,7 +205,7 @@ type mw struct {
 	basicOp
 	addr uint16
 	data uint8
-	f    func(*z80)
+	f    func(cpu *z80)
 }
 
 func (ops *mw) tick(cpu *z80) {
@@ -221,16 +261,17 @@ func (ops *out) tick(cpu *z80) {
 
 // -------------------------------------------------------------
 
+type z80EXECf func(cpu *z80)
 type exec struct {
 	basicOp
 	l byte
-	f z80f
+	f z80EXECf
 }
 
 func (ops *exec) tick(cpu *z80) {
 	ops.t++
 	if ops.t == ops.l {
-		ops.f(cpu, cpu.fetched)
+		ops.f(cpu)
 		ops.done = true
 	}
 }
